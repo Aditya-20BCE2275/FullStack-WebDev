07e67fe0a19e55298e8d14392d62d988780ac8bb
Aditya-20BCE2275 <amohite7100@gmail.com>
2022-07-13 21:39:59 +0530

    Drumkit miniproj update

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,7 +1,27 @@
+var n = document.querySelectorAll(".drum").length;
 
-document.querySelector("button").addEventListener("click",handleclick);
-
-function handleclick()
-{
-    alert("i got clicked");
+for (var i = 0; i < n; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", (i) => {
+        var sound;
+        if (i <= 3) {
+            sound = "sounds/tom-" + (i + 1) + ".mp3";
+        }
+        else {
+            switch (i) {
+                case 4:
+                    sound = "sounds/crash.mp3";
+                    break;
+                case 5:
+                    sound = "sounds/kick-bass.mp3";
+                    break;
+                case 6:
+                    sound = "sounds/snare.mp3";
+                    break;
+                // default:
+                //     sound = "sounds/tom-1.mp3";
+            }
+        }
+        var soundp = new Audio(sound);
+        soundp.play();
+    });
 }
