1839ec69ee969f2e315b898658fd8a6a6a225da6
Aditya-20BCE2275 <amohite7100@gmail.com>
2022-07-14 00:08:14 +0530

    Update index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,27 +1,59 @@
 var n = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < n; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", (i) => {
-        var sound;
-        if (i <= 3) {
-            sound = "sounds/tom-" + (i + 1) + ".mp3";
-        }
-        else {
-            switch (i) {
-                case 4:
-                    sound = "sounds/crash.mp3";
-                    break;
-                case 5:
-                    sound = "sounds/kick-bass.mp3";
-                    break;
-                case 6:
-                    sound = "sounds/snare.mp3";
-                    break;
-                // default:
-                //     sound = "sounds/tom-1.mp3";
-            }
-        }
-        var soundp = new Audio(sound);
-        soundp.play();
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+            var ih = this.innerHTML;
+            makesound(ih);
+            buttonActive(ih);
     });
+}
+
+document.addEventListener("keydown", function (event) {
+    makesound(event.key);
+    buttonActive(event.key);
+});
+
+function makesound(key) {
+    switch (key) {
+        case 'w':
+            var sound = new Audio("sounds/tom-1.mp3");
+            sound.play();
+            break;
+        case 'a':
+            var sound = new Audio("sounds/tom-2.mp3");
+            sound.play();
+            break;
+        case 's':
+            var sound = new Audio("sounds/tom-3.mp3");
+            sound.play();
+            break;
+        case 'd':
+            var sound = new Audio("sounds/tom-4.mp3");
+            sound.play();
+            break;
+        case 'j':
+            var sound = new Audio("sounds/crash.mp3");
+            sound.play();
+            break;
+        case 'k':
+            var sound = new Audio("sounds/kick-bass.mp3");
+            sound.play();
+            break;
+        case 'l':
+            var sound = new Audio("sounds/snare.mp3");
+            sound.play();
+            break;
+        default:
+            alert("Something went wrong pls try again later");
+            break;
+    }
+}
+
+function buttonActive(currentkey)
+{
+    var k=document.querySelector("."+currentkey);
+    k.classList.add("pressed");
+    setTimeout(function(){
+        k.classList.remove("pressed");
+    },200)
 }
